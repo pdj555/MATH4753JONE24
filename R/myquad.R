37b8ec3b85f48da96c9edbecadf197ba8805075f
preston jones <preston.d.jones-1@ou.edu>
2024-01-24 11:15:46 -0600

    init commit

---
+++ R/myquad.R
@@ -0,0 +1,12 @@
+#' A quadratic function
+#'
+#' @param x a number vector
+#'
+#' @return a numeric vector
+#' @export
+#'
+#' @examples
+#' myquad(x = 1:10)
+myquad <- function(x){
+  x^2 - 5*x + 6
+}
