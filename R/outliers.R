970c989814c3875ddffa7dc0d3aecbcdbb7c950a
preston jones <preston.d.jones-1@ou.edu>
2024-02-09 23:52:53 -0600

    outliers improved

--- R/outliers.R
+++ R/outliers.R
@@ -1,26 +1,32 @@
 #' Find Outliers
 #'
-#' @param I variable in vector
+#' Identifies outliers in a numeric vector based on z-scores. Outliers are defined as observations with z-scores greater than 3, and potential outliers are those with z-scores between 2 and 3.
 #'
-#' @return z values, z values greater than 3, z values that are possible outliers, I values corresponding to these z values
+#' @param I Numeric vector for which to find outliers.
+#' @return A list containing:
+#' \itemize{
+#'   \item{z}{All z-scores of the input vector.}
+#'   \item{outliers}{Z-scores greater than 3.}
+#'   \item{outlier_values}{I values corresponding to z-scores greater than 3.}
+#'   \item{potential_outliers}{Z-scores between 2 and 3.}
+#'   \item{potential_outlier_values}{I values corresponding to z-scores between 2 and 3.}
+#' }
 #' @export
 #'
 #' @examples
-#' outliers(I = I.df$IRON)
+#' outliers(I = c(1, 2, 3, 4, 100))
 outliers <- function(I){
   #Standardize(transform) into z values
-  z=(I-mean(I))/sd(I)
+  z <- (I - mean(I)) / sd(I)
 
-  # Find the z values greater than 3 in size
-  z[abs(z)>3]
+  # Prepare the list to return
+  results <- list(
+    z = z,
+    outliers = z[abs(z) > 3],
+    outlier_values = I[abs(z) > 3],
+    potential_outliers = z[abs(z) >= 2 & abs(z) <= 3],
+    potential_outlier_values = I[abs(z) >= 2 & abs(z) <= 3]
+  )
 
-  # Find the I values corresponding to these z values
-  I[abs(z)>3]
-
-  # Find the values of z that are possible outliers
-  z[abs(z)>=2 & abs(z)<=3]
-
-  #Find the values of I which are possible outliers
-  I[abs(z)>=2 & abs(z)<=3]
+  return(results)
 }
-
